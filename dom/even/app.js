06916b42feb1148c03899ad44ca677ecfa25d4c6
nazmulaxc <nazmulaxc@gmail.com>
2022-09-21 02:08:50 +0600

    added

--- dom/even/app.js
+++ dom/even/app.js
@@ -1,4 +1,9 @@
 function undo(){
     let catchs = document.getElementsByClassName('ch');
     console.log(catchs);
-}
+};
+
+//Notes;
+//Script a functioner nume hobe ;
+// index.html a call kora hoy ;
+// noi page load howar agei call hoye jabe;
